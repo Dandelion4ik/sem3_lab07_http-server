574d5d790280fa8b1d94ac86bd42a5a3cbe502c8
Dandelion4ik <Kaviandr@yandex.ru>
2021-01-26 09:47:48 +0300

    end_03

--- sources/source.cpp
+++ sources/source.cpp
@@ -199,3 +199,7 @@ int run_server(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 }
+// Using: ./cmake-build-debug/tests 0.0.0.0 8080
+int main(int argc, char* argv[]) {
+ return Run_server(argc, argv);
+}
